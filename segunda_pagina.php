23223ac9b62b323f84c8e23e37c4b1111d707198
jaesalberto2 <jaesalberto2@gmail.com>
2024-12-17 10:27:58 -0600

    variables

---
+++ segunda_pagina.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+    print "Bienvenidos al curso PHP <br>";//imprime y hace un salto de linea
+    print "Hola alumnos <br>";
+    print "Hasta el proximo video<br>";
+    $nombre="Juan";
+    $edad=18;
+    print "El nombre del usuario es: ".$nombre."<br>";
+    print "El nombre del usuario es: $nombre <br>";
+    print 'El nombre del usuario es: $nombre <br>';
+    print 'El nombre del usuario es:  '.$nombre." y su edad es: ".$edad."<br>";
+    echo $nombre, $edad;
+    echo "<br>";
+    
+
+    /*
+    hgjhglgkhjghjgkh
+    jhkhgjhgh
+    */
+    ?>
+    
+</body>
+</html>
